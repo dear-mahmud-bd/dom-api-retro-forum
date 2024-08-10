15508df70179feb67e5ddf3a8d4111ad391f8c92
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2024-08-10 23:29:42 +0600

    latest post js part

--- file/JS/app.js
+++ file/JS/app.js
@@ -2,6 +2,7 @@
 // latest post: https://openapi.programming-hero.com/api/retro-forum/latest-posts 
 
 // all-post-container
+// latest-post-container
 
 
 
@@ -15,13 +16,13 @@ const loadAllPost = async () => {
 loadAllPost();
 
 const displayAllPost = (allPost) => {
-    console.log(allPost);
+    // console.log(allPost);
     const allPostContainer = document.getElementById('all-post-container');
     // console.log(allPostContainer);
-    // allPostContainer.textContent = '';
+    allPostContainer.textContent = '';
 
     allPost.forEach(post => {
-        console.log(post);
+        // console.log(post);
         // console.log(post.isActive);
 
         const postCard = document.createElement('div');
@@ -69,6 +70,50 @@ const displayAllPost = (allPost) => {
 
 };
 
+
+const loadLatestPost = async () =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
+    const data = await res.json();
+    const allPost = data;
+    displayLatestPost(allPost);
+};
+const displayLatestPost = (allPost) =>{
+    console.log(allPost);
+    const latestPostContainer = document.getElementById('latest-post-container');
+    // latestPostContainer.innerText = '';
+    allPost.forEach(post => {
+        console.log(post);
+        const postCard = document.createElement('div');
+        postCard.classList = `card bg-white border rounded-lg overflow-hidden`;
+        postCard.innerHTML = `
+            <img class="m-4 h-48 w-auto rounded-lg" src="${post.cover_image}" alt="">          
+            <div class="p-4">
+                <div class="flex items-center text-sm text-gray-500 mb-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M8 7V3m8 4V3M3 11h18M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>
+                    ${post.author['posted_date']? post.author['posted_date']: 'No publish date'}
+                </div>
+                <h2 class="text-lg font-semibold mb-2">${post.title}</h2>
+                <p class="text-gray-600 mb-4">${post.description}</p>
+                <div class="flex items-center">
+                    <div class="w-10 h-10 rounded-full bg-gray-300 mr-3">
+                        <img class="rounded-full" src="${post.profile_image}" />
+                    </div>
+                    <div>
+                        <p class="text-sm font-semibold">${post.author['name']}</p>
+                        <p class="text-xs text-gray-500">${post.author['designation'] ? post.author['designation']: 'Unknown'}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+        latestPostContainer.appendChild(postCard);
+    });
+};
+
+loadLatestPost();
 
 
 
